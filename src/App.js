27319a142e8ef556bd8e19b6b3f03ecd3cb6fc34
Pramadha Yaramakala <pramadhayaramakala@Pramadhas-MacBook-Pro.local>
2024-03-28 22:08:52 +0530

    budgetplanner

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import LandingScreen from './screens/LandingScreen';
+import { createContext, useState } from 'react';
+import BudgetContext from './components/BudgetContext';
 
 function App() {
+  const totalBudget = 2000;
+  const [totalSpend, setTotalSpend] = useState(0);
+  const [expenses, setExpenses] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BudgetContext.Provider value={{totalBudget,totalSpend,setTotalSpend, expenses,setExpenses}}>
+       <div id="logo-container">
+        <img src={'https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color/254000/71-512.png'} alt="Logo" />
+      </div>
+      <LandingScreen/>
+    </BudgetContext.Provider>
   );
 }
 
